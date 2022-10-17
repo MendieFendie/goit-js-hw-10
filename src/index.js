6e0ad533a44ad3daaa024cfed746568baef8aa08
Oleh Savaryn <olegsavaryn@gmail.com>
2022-10-17 21:33:18 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './css/styles.css';
 import './js/fetchCountries';
-
+// import './js/countryList.hbs';
 const DEBOUNCE_DELAY = 300;
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,28 +1,52 @@
 const debounce = require('lodash.debounce');
-const countryList = document.querySelector('country-list');
-const countryInfo = document.querySelector('country-info');
+import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+import countryListHbs from '../templates/countryList.hbs';
+import countryInfoHbs from '../templates/countryInfo.hbs';
+
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const searchString = document.querySelector('#search-box');
 
 const input = document.querySelector('input');
 
 input.addEventListener('input', debounce(searchValue, 300));
 let name = '';
 function searchValue(e) {
-  name = e.target.value;
+  name = e.target.value.trim();
   console.log(name);
   responce(name);
 }
 
 function responce(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flag,languages`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flag,flags,languages`
   )
     .then(responce => {
       return responce.json();
     })
-    .then(renderCountryCard)
+    .then(renderCountryCardList)
     .catch(error => console.log(error));
 }
 
-function renderCountryCard(countries) {
-  console.dir(countries);
+function renderCountryCardList(countries) {
+  if (searchString.value === '') {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    Notiflix.Notify.warning('Memento te hominem esse');
+  } else if (countries.length > 10) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    let markup = countryListHbs(countries);
+    countryInfo.innerHTML = markup;
+    countryList.innerHTML = '';
+  } else {
+    let markup = countryInfoHbs(countries);
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = markup;
+  }
 }
